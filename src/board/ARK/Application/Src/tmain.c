ec9123a6d4dc1f62d13ba8cacd1098f7b0d3f247
SeresHotes <sereshotes@gmail.com>
2020-02-29 19:40:36 +0300

    INA219 работает
    Я нашел баг в драйвере Кирилла в файле ina219.c строка 125. Кирилл
    полагал, что значение в даташите - это значение переменной Cal.
    Но в даташите написано, что Cal - значение регистра, а значит
    проводить дополнительные действия по сдвигу (значения переменной
    выравнивается не по нулевому биту, а по первому) нет нужды.

--- src/board/ARK/Application/Src/tmain.c
+++ src/board/ARK/Application/Src/tmain.c
@@ -3,59 +3,30 @@
 #include <stdio.h>
 
 #include "ds18b20.h"
-#define DS18B20_COUNT 2
+#include "ina_helper.h"
+#include "i2c.h"
+
 
 int tmain(void) {
 
     printf("tmain start\n");
 
-
-    onewire_t how = {0};
-    //onewire_pin_init(&how, GPIOB, GPIO_PIN_10);
-    onewire_Init(&how, GPIOB, GPIO_PIN_0);
-
-    onewire_Reset(&how);
-
-    HAL_Delay(100);
-
-    ds18b20_config_t hds[DS18B20_COUNT];
-    int ds_count = 0;
-
-
-    onewire_ResetSearch(&how);
-    for (ds_count = 0; ds_count < DS18B20_COUNT; ds_count++) {
-        if (!onewire_Next(&how)) {
-            break;
-        }
-
-        onewire_GetFullROM(&how, &hds[ds_count].rom);
-        hds[ds_count].how = &how;
-        hds[ds_count].resolution = ds18b20_Resolution_12bits;
-        ds18b20_SetResolution(&hds[ds_count], hds[ds_count].resolution);
-    }
-    printf("DS count: %d\n", ds_count);
-
-    ds18b20_StartAll(&hds[0]);
+    ina219_t hina = {0};
+    _ina_init(&hina, &hi2c1, INA219_I2CADDR_A1_GND_A0_GND << 1);
 
     printf("while start\n");
     while(1) {
-        float t = 0.1;
+        float current = 0, power = 0;
+        ina219_secondary_data_t sd;
+        ina219_primary_data_t pd;
+        ina219_read_all(&hina, &pd, &sd);
+        //_ina_read(&hina, &current, &power);
+        float cur = _ina_current_convert(&hina, sd.current);
+        float pow = _ina_power_convert(&hina, sd.power);
+        float vbs = _ina_bus_voltage_convert(&hina, pd.busv);
+        float vsh = _ina_shunt_voltage_convert(&hina, pd.shuntv);
+        printf("I: %0.2f  Us: %0.2f  U: %0.2f  P: %0.2f\n", cur * 1000, vsh, vbs * 1000 , pow * 1000);
 
-        if (onewire_ReadBit(hds->how)) {
-            for (int i = 0; i < ds_count; i++) {
-                ds18b20_Read(&hds[i], &t);
-                printf("[%d]: %0.2f\t", i, t);
-            }
-            printf("\n");
-            ds18b20_StartAll(&hds[0]);
-        }
-
-    }
-    volatile int index = 0;
-
-    while (1) {
-        printf("Count of 't': %d\n", index);
-        index += 2;
     }
 
     return 0;
