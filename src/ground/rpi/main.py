825cfc609f0fdeeff94b7274e52e569e026e776e
Agnus290 <tenebris.agnus@yandex.ru>
2020-03-07 20:01:41 +0300

    Исправление ошибки с предыдущим комитом

--- src/ground/rpi/main.py
+++ src/ground/rpi/main.py
@@ -1,12 +1,13 @@
 import i2cdev
 import ctypes
+import time
 
 srtela_ms_rpi_c = ctypes.CDLL("./native/build/libstrela-ms-rpi.so")
 
 PORT_I2C = 1
 I2C_TIMEOUT = 1
 
-LIS3MDL_ADRESS = 0x1C
+LIS3MDL_ADRESS = 0x1e
 LSM6DS3_ADRESS = 0x6B
 
 stmdev_ctx_func_type = ctypes.CFUNCTYPE(ctypes.c_uint32,
@@ -15,31 +16,39 @@ stmdev_ctx_func_type = ctypes.CFUNCTYPE(ctypes.c_uint32,
                                         ctypes.POINTER(ctypes.c_uint8),
                                         ctypes.c_uint16)
 
-srtela_ms_rpi_c.lis3mdl_init.argtypes = (stmdev_ctx_func_type, stmdev_ctx_func_type)
+class stmdev_ctx_t(ctypes.Structure):
+    _fields_ = [('write_reg', stmdev_ctx_func_type),
+                ('read_reg', stmdev_ctx_func_type),
+                ('handle', ctypes.c_void_p)]
+
+srtela_ms_rpi_c.lis3mdl_init.argtypes = (ctypes.POINTER(stmdev_ctx_t),)
 srtela_ms_rpi_c.lis3mdl_init.restype = ctypes.c_int32
 
-srtela_ms_rpi_c.lis3mdl_get_data_G.argtypes = (ctypes.POINTER(ctypes.c_float),)
+srtela_ms_rpi_c.lis3mdl_get_data_G.argtypes = (ctypes.POINTER(stmdev_ctx_t), ctypes.POINTER(ctypes.c_float),)
 srtela_ms_rpi_c.lis3mdl_get_data_G.restype = ctypes.c_int32
 
-srtela_ms_rpi_c.lsm6ds3_init.argtypes = (stmdev_ctx_func_type, stmdev_ctx_func_type)
+srtela_ms_rpi_c.lsm6ds3_init.argtypes = (ctypes.POINTER(stmdev_ctx_t),)
 srtela_ms_rpi_c.lsm6ds3_init.restype = ctypes.c_int32
 
-srtela_ms_rpi_c.lsm6ds3_get_accel_data_mg.argtypes = (ctypes.POINTER(ctypes.c_float),)
+srtela_ms_rpi_c.lsm6ds3_get_accel_data_mg.argtypes = (ctypes.POINTER(stmdev_ctx_t), ctypes.POINTER(ctypes.c_float),)
 srtela_ms_rpi_c.lsm6ds3_get_accel_data_mg.restype = ctypes.c_int32
 
-class Lis3mdl_context():
+
+class Stmdev_i2c_context():
     def __init__(self, i2c, i2c_addr):
         self.i2c_line = i2c
         self.i2c_addr = i2c_addr
         self.last_error = None
-
+        read = lambda handle, reg, bufp, buf_len: self.read(handle, reg, bufp, buf_len)
+        write = lambda handle, reg, bufp, buf_len: self.write(handle, reg, bufp, buf_len)
+        self.context = stmdev_ctx_t(stmdev_ctx_func_type(write),
+                                    stmdev_ctx_func_type(read),
+                                    0)
     def read(self, handle, reg, bufp, buf_len):
         self.last_error = None
         try:
             self.i2c_line.set_addr(self.i2c_addr)
-            reg |= 0x40
-            self.i2c_line.write(bytes([reg]))
-            buf = self.i2c_line.read(buf_len)
+            buf = self.i2c_line.rdwr(bytes([reg]), buf_len)
             for i in range(buf_len):
                 bufp[i] = buf[i]
         except Exception as e:
@@ -48,61 +57,6 @@ class Lis3mdl_context():
         return 0
 
     def write(self, handle, reg, bufp, buf_len):
-        self.last_error = None
-        try:
-            self.i2c_line.set_addr(self.i2c_addr)
-            reg |= 0xC0
-            self.i2c_line.write(bytes([reg]))
-            buf = list()
-            for i in range(buf_len):
-                buf.append(bufp[i])
-            self.i2c_line.write(bytes(buf))
-        except Exception as e:
-            self.last_error = e
-            return 1
-        return 0
-
-class Lsm6ds3_context():
-
-    def __init__(self, i2c, i2c_addr):
-        self.i2c_line = i2c
-        self.i2c_addr = i2c_addr
-        self.last_error = None
-
-    def setup(self):
-        read = lambda handle, reg, bufp, buf_len: self.read(handle, reg, bufp, buf_len)
-        write = lambda handle, reg, bufp, buf_len: self.write(handle, reg, bufp, buf_len)
-        error = srtela_ms_rpi_c.lsm6ds3_init(stmdev_ctx_func_type(read), 
-                                             stmdev_ctx_func_type(write))
-        if error:
-            raise RuntimeError("lsm6ds3_init returned nonzero error: %d" % error) from self.last_error
-
-    def get_accel_data_mg(self):
-        accel_buf = (ctypes.c_float * 3)()
-        # print("addr = 0x%02X" % ctypes.addressof(accel_buf))
-        error = srtela_ms_rpi_c.lsm6ds3_get_accel_data_mg(ctypes.cast(ctypes.byref(accel_buf), ctypes.POINTER(ctypes.c_float)))
-        if error:
-            raise RuntimeError("lsm6ds3_init returned nonzero error: %d" % error) from self.last_error
-        return accel_buf
-
-    def read(self, handle, reg, bufp, buf_len):
-        self.last_error = None
-        try:
-            self.i2c_line.set_addr(self.i2c_addr)
-            print("reg = 0x%02X, bufp = %s" % (reg, ctypes.cast(bufp, (ctypes.c_void_p))))
-            buf = self.i2c_line.rdwr(bytes([reg]), buf_len)
-            print(buf, buf_len)
-            bufp[0] = buf[0]
-            # for i in range(buf_len):
-            #     bufp[i] = buf[i]
-
-        except Exception as e:
-            self.last_error = e
-            return 1
-        return 0
-
-    def write(self, handle, reg, bufp, buf_len):
-        return 0
         self.last_error = None
         try:
             self.i2c_line.set_addr(self.i2c_addr)
@@ -115,35 +69,54 @@ class Lsm6ds3_context():
             return 1
         return 0
 
+
+class Lis3mdl(Stmdev_i2c_context):
+    def setup(self):
+        error = srtela_ms_rpi_c.lis3mdl_init(ctypes.byref(self.context))
+        if error:
+            raise RuntimeError("lis3mdl_init returned nonzero error: %d" % error) from self.last_error
+
+    def get_mag_data_G(self):
+        mag_buf = (ctypes.c_float * 3)()
+        error = srtela_ms_rpi_c.lis3mdl_get_data_G(ctypes.byref(self.context), ctypes.cast(ctypes.byref(mag_buf), ctypes.POINTER(ctypes.c_float)))
+        if error:
+            raise RuntimeError("lis3mdl_get_data_G returned nonzero error: %d" % error) from self.last_error
+        return list(mag_buf)
+
+
+class Lsm6ds3(Stmdev_i2c_context):
+    def setup(self):
+        error = srtela_ms_rpi_c.lsm6ds3_init(ctypes.byref(self.context))
+        if error:
+            raise RuntimeError("lsm6ds3_init returned nonzero error: %d" % error) from self.last_error
+
+    def get_accel_data_mg(self):
+        accel_buf = (ctypes.c_float * 3)()
+        error = srtela_ms_rpi_c.lsm6ds3_get_accel_data_mg(ctypes.byref(self.context), ctypes.cast(ctypes.byref(accel_buf), ctypes.POINTER(ctypes.c_float)))
+        if error:
+            raise RuntimeError("lsm6ds3_get_accel_data_mg returned nonzero error: %d" % error) from self.last_error
+        return list(accel_buf)
+
 if __name__ == '__main__':
     i2c = i2cdev.I2C(PORT_I2C)
     i2c.set_timeout(I2C_TIMEOUT)
 
-    
-    #mag_buf = data_buf_type()
+    lis3mdl = Lis3mdl(i2c, LIS3MDL_ADRESS)
+    lis3mdl.setup()    
 
-    #lis3mdl = Lis3mdl_context(i2c, LIS3MDL_ADRESS)
-    #read = lambda handle, reg, bufp, buf_len: lis3mdl.read(handle, reg, bufp, buf_len)
-    #write = lambda handle, reg, bufp, buf_len: lis3mdl.write(handle, reg, bufp, buf_len)
-    #error = srtela_ms_rpi_c.lis3mdl_init(stmdev_ctx_func_type(read), 
-    #                                     stmdev_ctx_func_type(write))
-    #if (error):
-    #    raise lis3mdl.last_error
-
-    lsm6ds3 = Lsm6ds3_context(i2c, LSM6DS3_ADRESS)
-    lsm6ds3.setup()    
-
+    #lsm6ds3 = Lsm6ds3(i2c, LSM6DS3_ADRESS)
+    #lsm6ds3.setup()    
 
     while True:
-        #error = srtela_ms_rpi_c.lis3mdl_get_data_G(mag_buf)
-        #if (error != 0):
-        #    print("Lis3mdl error : " + str(error))
+        mag_buf = lis3mdl.get_mag_data_G()
+        #accel_buf = lsm6ds3.get_accel_data_mg()
 
-        accel_buf = lsm6ds3.get_accel_data_mg()
-        #print("Mag  :")
-        #print(mag_buf)
-        print("Accel:")
-        print(accel_buf)
+        time.sleep(0.5)
+
+        print("Mag  :")
+        print(mag_buf)
+        #print("Accel:")
+        #print(accel_buf)
 
 
     i2c.close()
--- src/ground/rpi/native/src/lib-strela-ms-rpi/strela-ms-rpi.c
+++ src/ground/rpi/native/src/lib-strela-ms-rpi/strela-ms-rpi.c
@@ -12,100 +12,80 @@ typedef enum {
 	ERROR_UNKNOWN_ID = -1
 } error_t;
 
-stmdev_ctx_t lis3mdl_context;
-stmdev_ctx_t lsm6ds3_context;
-
-int32_t lis3mdl_init (stmdev_write_ptr read, stmdev_write_ptr write){
+int32_t lis3mdl_init (stmdev_ctx_t *lis3mdl_context){
 	int32_t error = 0;
 	uint8_t reset = 1;
 	uint8_t id = 0;
 
-	// Interface context initialization
-	lis3mdl_context.write_reg = write;
-	lis3mdl_context.read_reg = read;
-	lis3mdl_context.handle = 0;
-
 	// Reset
-	// Reset
-	lsm6ds3_reg_t ctrl3c = {0};
-	ctrl3c.ctrl3_c.sw_reset = 1;
-	error = lsm6ds3_write_reg(&lsm6ds3_context, LSM6DS3_CTRL3_C, &ctrl3c.byte, 1);
+    error = lis3mdl_reset_set(lis3mdl_context, PROPERTY_ENABLE);
 	if (error) return error;
 	usleep(50*1000);
 
 	// Check id
-	error = lis3mdl_device_id_get(&lis3mdl_context, &id);
+	error = lis3mdl_device_id_get(lis3mdl_context, &id);
 	if (error) return error;
 	if (id != LIS3MDL_ID) return ERROR_UNKNOWN_ID;
 
 	// Set configuration
-	error = lis3mdl_block_data_update_set(&lis3mdl_context, PROPERTY_DISABLE);
+	error = lis3mdl_block_data_update_set(lis3mdl_context, PROPERTY_ENABLE);
 	if (error) return error;
-	error = lis3mdl_data_rate_set(&lis3mdl_context, LIS3MDL_HP_40Hz);
+	error = lis3mdl_data_rate_set(lis3mdl_context, LIS3MDL_HP_40Hz);
 	if (error) return error;
-	error = lis3mdl_full_scale_set(&lis3mdl_context, LIS3MDL_16_GAUSS);
+	error = lis3mdl_full_scale_set(lis3mdl_context, LIS3MDL_16_GAUSS);
 	if (error) return error;
-	error = lis3mdl_operating_mode_set(&lis3mdl_context, LIS3MDL_CONTINUOUS_MODE);
+	error = lis3mdl_operating_mode_set(lis3mdl_context, LIS3MDL_CONTINUOUS_MODE);
 	return error;
 }
 
-int32_t lis3mdl_get_data_G (float* data){
+int32_t lis3mdl_get_data_G (stmdev_ctx_t *lis3mdl_context, float* mag){
 	uint8_t data_raw [6];
 	uint8_t error;
 
-	error = lis3mdl_magnetic_raw_get(&lis3mdl_context, data_raw);
+	error = lis3mdl_magnetic_raw_get(lis3mdl_context, data_raw);
 	if (error) return error;
-	data[0] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[1] << 8) | data_raw[0]));
-	data[1] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[3] << 8) | data_raw[2]));
-	data[2] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[5] << 8) | data_raw[4]));
+	mag[0] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[1] << 8) | data_raw[0]));
+	mag[1] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[3] << 8) | data_raw[2]));
+	mag[2] = LIS3MDL_FROM_FS_16G_TO_G(((data_raw[5] << 8) | data_raw[4]));
 	return 0;
 }
 
-int32_t lsm6ds3_init(stmdev_write_ptr read, stmdev_write_ptr write){
+int32_t lsm6ds3_init(stmdev_ctx_t *lsm6ds3_context){
 	int32_t error = 0;
 	uint8_t reset = 1;
 	uint8_t id = 0;
 
-	// Interface context initialization
-	lsm6ds3_context.write_reg = write;
-	lsm6ds3_context.read_reg = read;
-	lsm6ds3_context.handle = 0;
-
 	// Reset
 	lsm6ds3_reg_t reg = {0};
 	reg.ctrl3_c.sw_reset = 1;
-	error = lsm6ds3_write_reg(&lsm6ds3_context, LSM6DS3_CTRL3_C, &reg.byte, 1);
+	error = lsm6ds3_write_reg(lsm6ds3_context, LSM6DS3_CTRL3_C, &reg.byte, 1);
 	if (error) return error;
 	usleep(50*1000);
 
 	// Check id
-	error = lsm6ds3_device_id_get(&lsm6ds3_context, &id);
+	error = lsm6ds3_device_id_get(lsm6ds3_context, &id);
 	if (error) return error;
 	if (id != LSM6DS3_ID) return ERROR_UNKNOWN_ID;
 
 	// Set configuration
-	error = lsm6ds3_fifo_mode_set(&lsm6ds3_context, PROPERTY_DISABLE);
+	error = lsm6ds3_block_data_update_set(lsm6ds3_context, PROPERTY_ENABLE);
 	if (error) return error;
-	error = lsm6ds3_block_data_update_set(&lsm6ds3_context, PROPERTY_DISABLE);
+	error = lsm6ds3_xl_full_scale_set(lsm6ds3_context, LSM6DS3_4g);
 	if (error) return error;
-	error = lsm6ds3_xl_full_scale_set(&lsm6ds3_context, LSM6DS3_4g);
+	error = lsm6ds3_gy_full_scale_set(lsm6ds3_context, LSM6DS3_1000dps);
 	if (error) return error;
-	error = lsm6ds3_gy_full_scale_set(&lsm6ds3_context, LSM6DS3_1000dps);
+	error = lsm6ds3_xl_data_rate_set(lsm6ds3_context, LSM6DS3_XL_ODR_104Hz);
 	if (error) return error;
-	error = lsm6ds3_xl_data_rate_set(&lsm6ds3_context, LSM6DS3_XL_ODR_104Hz);
-	if (error) return error;
-	error = lsm6ds3_gy_data_rate_set(&lsm6ds3_context, LSM6DS3_GY_ODR_104Hz);
-	if (error) return error;
-	error = lsm6ds3_xl_filter_analog_set(&lsm6ds3_context, LSM6DS3_ANTI_ALIASING_200Hz);
+	error = lsm6ds3_gy_data_rate_set(lsm6ds3_context, LSM6DS3_GY_ODR_104Hz);
 	return error;
 }
 
 
-int32_t lsm6ds3_get_accel_data_mg(float* accel)
+int32_t lsm6ds3_get_accel_data_mg(stmdev_ctx_t *lsm6ds3_context, float* accel)
 {
 	uint8_t data_raw [6];
 	int32_t error;
-	error = lsm6ds3_acceleration_raw_get(&lsm6ds3_context, data_raw);
+	error = lsm6ds3_acceleration_raw_get(lsm6ds3_context, data_raw);
 	if (error) return error;
 	accel[0] = lsm6ds3_from_fs4g_to_mg((data_raw[1] << 8) | data_raw[0]);
 	accel[1] = lsm6ds3_from_fs4g_to_mg((data_raw[3] << 8) | data_raw[2]);
