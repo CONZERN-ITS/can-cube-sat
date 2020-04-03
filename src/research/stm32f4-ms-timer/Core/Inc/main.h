d8601e9ca4d908d96b4cabded77060869882bf33
snork2 <strange.snork@gmail.com>
2020-04-03 13:29:04 +0300

    Тест проект для работы с PPS

--- src/research/stm32f4-ms-timer/Core/Inc/main.h
+++ src/research/stm32f4-ms-timer/Core/Inc/main.h
@@ -60,8 +60,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define qqq_Pin GPIO_PIN_12
-#define qqq_GPIO_Port GPIOD
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
