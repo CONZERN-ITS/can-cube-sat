50e706defc8edbdd315751892ec99889dcfb62e7
snork2 <strange.snork@gmail.com>
2020-04-04 19:40:17 +0300

    Мучаем мавлинк

--- src/common/mavlink/reinstall-pymavlink.sh
+++ src/common/mavlink/reinstall-pymavlink.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Определяем путь до родительского каталога этого скрипта
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -10,11 +10,7 @@ cd $DIR/pymavlink
 /usr/bin/env python3 setup.py clean --all
 
 # Собираем whl
-if ! /usr/bin/env python3 setup.py bdist_wheel
-then
-	echo "Не могу собрать wheel из pymavlink"
-	exit 1
-fi
+/usr/bin/env python3 setup.py bdist_wheel
 
 # находим собранный whl в папке dist
 wheel_path=$(find dist -name pymavlink*.whl)
@@ -25,10 +21,4 @@ then
 fi
 
 # Устанавливаем собранный whl в текущий активный питон
-if ! /usr/bin/env pip3 install -U $wheel_path
-then
-	echo "pymavlink установлен"
-	exit 0
-fi
-
-echo "Кажется, что-то пошло не так?"
+/usr/bin/env pip3 install -U $wheel_path
