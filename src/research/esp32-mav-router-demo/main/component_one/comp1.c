960cfbbce107fea8f6d5edbd649817e0a098bd22
SeresHotes <sereshotes@gmail.com>
2020-04-09 12:55:53 +0300

    Еще один вариант роутинга

--- src/research/esp32-mav-router-demo/main/component_one/comp1.c
+++ src/research/esp32-mav-router-demo/main/component_one/comp1.c
@@ -9,53 +9,6 @@
 
 #include "../router/router.h"
 
-
-#define ITS_COMP1_MAVCOMP_ID 10
-
-
-
-its_c1_rt_endpoint_t its_c1_rt_endpoint = { 0 };
-
-
-
-//! Фильтрация входящих сообщений и их предобработка
-/*! Эту функцию вызывают другие таски, поэтому для работы с контекстом нашего таска - нужно
- *  как-то синхронизироваться. Мы будем использовать очередь */
-static int accept_message(its_rt_endpoint_t * self_, const its_rt_sender_ctx_t * sender_ctx,
-		const mavlink_message_t * msg
-){
-	its_c1_rt_endpoint_t * self = (its_c1_rt_endpoint_t * )self_;
-
-	switch (msg->msgid)
-	{
-	case MAVLINK_MSG_ID_GPS_UBX_NAV_TIMEGPS:
-	case MAVLINK_MSG_ID_GPS_UBX_NAV_SOL:
-		// Эти сообщения мы хотим
-		if (sender_ctx->from_isr)
-		{
-			// Если нас дернули из прерывания, то ведем себя вот так
-			BaseType_t higher_prio_woken;
-			xQueueSendFromISR(self->task_input_queue, msg, &higher_prio_woken);
-			if (higher_prio_woken)
-				portYIELD_FROM_ISR();
-		}
-		else
-		{
-			// Если не из прерывания - не стесняемся
-			// FIXME: возможно оптравитель захочет нам сказать, что стеснятся как раз надо?
-			xQueueSend(self->task_input_queue, msg, portMAX_DELAY);
-		}
-		break;
-
-	default:
-		//Все остальные не хотим
-		break;
-	}
-
-	return 0;
-}
-
-
 //! Обработка пришедшего сообщения
 static void process_message(const mavlink_message_t * msg)
 {
@@ -86,12 +39,18 @@ void its_c1_task_entry(void * arg)
 	xQueueHandle input_queue = xQueueCreate(10, sizeof(mavlink_message_t));
 	assert(input_queue);
 
-	its_c1_rt_endpoint.base.acceptor = accept_message;
+	//its_c1_rt_endpoint.base.acceptor = accept_message;
 	its_c1_rt_endpoint.task_input_queue = input_queue;
 
 	its_rt_sender_ctx_t sctx = {
 			.from_isr = 0
 	};
+	const int queueSize = 10;
+	QueueHandle_t qh = xQueueCreate(queueSize, MAVLINK_MAX_PACKET_LEN);
+	its_rt_task_identifier tid = {
+			.queue = qh
+	};
+	its_rt_register(MAVLINK_MSG_ID_GPS_UBX_NAV_SOL, tid);
 
 	for (;;)
 	{
@@ -113,9 +72,9 @@ void its_c1_task_entry(void * arg)
 		// Лезем к дачтикам и выясняем
 
 		// FIXME 42 заменить на какой-нибудь макрос с идентификатором esp32 как системы
-		mavlink_msg_power_status_pack(42, ITS_COMP1_MAVCOMP_ID, &msg, vcc, vservo, flags);
+		mavlink_msg_power_status_pack(42, 0, &msg, vcc, vservo, flags);
 
 		// отправляем пакет в маршрутизацию
-		its_rt_route(&sctx, &msg);
+		its_rt_route(&sctx, &msg, portMAX_DELAY);
 	}
 }
--- src/research/esp32-mav-router-demo/main/router/router.h
+++ src/research/esp32-mav-router-demo/main/router/router.h
@@ -10,11 +10,29 @@
 
 #include "../mavlink/its/mavlink.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 #include "rt_sender_ctx.h"
 
 //! Функция - маршрутизатор
 /*! Эта функция отправляет пакеты всем абонентам. А абоненты решают нужны им эти пакеты или нет */
-void its_rt_route(const its_rt_sender_ctx_t * sender_ctx, const mavlink_message_t * msg);
+void its_rt_route_from_isr(
+		const its_rt_sender_ctx_t * sender_ctx,
+		const mavlink_message_t * msg
+);
+
+typedef struct its_rt_task_identifier {
+	QueueHandle_t queue;
+}its_rt_task_identifier;
+
+void its_rt_route(
+		const its_rt_sender_ctx_t * sender_ctx,
+		const mavlink_message_t * msg,
+		TickType_t ticksToWaitForOne
+);
+int its_rt_register(int msg_id, its_rt_task_identifier task_id);
+void its_rt_uninit();
 
 
 #endif /* MAIN_ROUTER_ROUTER_H_ */
