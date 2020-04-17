87e89652a3248e247485337761d155693e7fd05a
dll31 <danila.litvinov31@yandex.ru>
2020-04-17 20:09:10 +0300

    Таймеры работают

--- src/board/SINS/src/stm32f4xx_hal_msp.c
+++ src/board/SINS/src/stm32f4xx_hal_msp.c
@@ -242,16 +242,16 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
 		/* Peripheral clock enable */
 		__HAL_RCC_TIM4_CLK_ENABLE();
 
-		__HAL_RCC_GPIOD_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_ENABLE();
 		/**TIM4 GPIO Configuration
-		PD13     ------> TIM4_CH2 для TRGI на сброс по PPS сигналу
+		PB7     ------> TIM4_CH2 для TRGI на сброс по PPS сигналу
 		*/
-		GPIO_InitStruct.Pin = GPIO_PIN_13;
+		GPIO_InitStruct.Pin = GPIO_PIN_7;
 		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
 		GPIO_InitStruct.Pull = GPIO_PULLDOWN;
 		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
 		GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
-		HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 	}
 	else if(htim_base->Instance==TIM6)
 	{
@@ -283,16 +283,16 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
 	}
 	else if(htim->Instance==TIM4)
 	{
-		__HAL_RCC_GPIOD_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_ENABLE();
 		/**TIM4 GPIO Configuration
-		PD12     ------> TIM4_CH1 // для питания TIM3 опорной частотой
+		PB6     ------> TIM4_CH1 // для питания TIM3 опорной частотой
 		*/
-		GPIO_InitStruct.Pin = GPIO_PIN_12;
+		GPIO_InitStruct.Pin = GPIO_PIN_6;
 		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-		GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
 		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
 		GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
-		HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 	}
 
 }
@@ -331,10 +331,10 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
 		__HAL_RCC_TIM4_CLK_DISABLE();
 
 		/**TIM4 GPIO Configuration
-		PD12     ------> TIM4_CH1
-		PD13     ------> TIM4_CH2
+		PB6     ------> TIM4_CH1
+		PB7     ------> TIM4_CH2
 		*/
-		HAL_GPIO_DeInit(GPIOD, GPIO_PIN_12|GPIO_PIN_13);
+		HAL_GPIO_DeInit(GPIOB, GPIO_PIN_6|GPIO_PIN_7);
 	}
 }
 
