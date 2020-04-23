db0246543567cb2486fc8970048d537d4bb2d1e6
Agnus290 <tenebris.agnus@yandex.ru>
2020-04-23 21:39:24 +0300

    К предыдущему коммиту

--- src/ground/laptop/source/__init__.py
+++ src/ground/laptop/source/__init__.py
@@ -1,3 +1,4 @@
 import os
 RES_ROOT = os.path.dirname(os.path.realpath(__file__))
 USER_SETTINGS_PATH = os.path.join(os.path.split(RES_ROOT)[0], "StrelA_MS.ini")
+LOG_FOLDER_PATH = os.path.join(os.path.split(RES_ROOT)[0], "log/")
