596e8e1e18e788bd8d794e4b4425401483c9128b
Agnus290 <tenebris.agnus@yandex.ru>
2020-07-14 19:36:04 +0300

    исправлена табуляция

--- src/ground/rpi/DM422.py
+++ src/ground/rpi/DM422.py
@@ -6,15 +6,15 @@ DIR_PIN = 14
 ENABLE_PIN = 16
 
 class DM422_control_client ():
-	def __init__ (self, pul_pin=PUL_PIN, dir_pin=DIR_PIN, enable_pin=ENABLE_PIN, gearbox_num=1, deg_per_step=1):
-		self.pul_pin = pul_pin
-		self.dir_pin = dir_pin
-		self.enable_pin = enable_pin
-		self.gearbox_num = gearbox_num
-		self.deg_per_step = deg_per_step
+    def __init__ (self, pul_pin=PUL_PIN, dir_pin=DIR_PIN, enable_pin=ENABLE_PIN, gearbox_num=1, deg_per_step=1):
+        self.pul_pin = pul_pin
+        self.dir_pin = dir_pin
+        self.enable_pin = enable_pin
+        self.gearbox_num = gearbox_num
+        self.deg_per_step = deg_per_step
 
-	def setup (self):
-		GPIO.setmode(GPIO.BOARD)
+    def setup (self):
+        GPIO.setmode(GPIO.BOARD)
 
         GPIO.setup(self.pul_pin, GPIO.OUT)
         GPIO.output(self.pul_pin, True)
@@ -26,28 +26,28 @@ class DM422_control_client ():
         GPIO.output(self.enable_pin, False)
 
     def rotate_using_angle (self, ang):
-    	steps = self.gearbox_num * ang / self.deg_per_step
-    	if steps > 0:
-    		self.rotate_using_steps(round(steps), False)
-    	else:
-    		self.rotate_using_steps(round(abs(steps)), True)
+        steps = self.gearbox_num * ang / self.deg_per_step
+        if steps > 0:
+            self.rotate_using_steps(round(steps), False)
+        else:
+            self.rotate_using_steps(round(abs(steps)), True)
 
     def rotate_using_steps (self, steps, direction=False):
-    	GPIO.output(self.pul_pin, True)
-    	GPIO.output(self.enable_pin, True)
-    	time.sleep(0.000005)
+        GPIO.output(self.pul_pin, True)
+        GPIO.output(self.enable_pin, True)
+        time.sleep(0.000005)
 
-    	GPIO.output(self.dir_pin, direction)
-    	time.sleep(0.000005)
+        GPIO.output(self.dir_pin, direction)
+        time.sleep(0.000005)
 
-    	for i in range(steps):
-    		GPIO.output(self.pul_pin, False)
-    		time.sleep(0.0000075)
-    		GPIO.output(self.pul_pin, True)
-    		time.sleep(0.0000075)
+        for i in range(steps):
+            GPIO.output(self.pul_pin, False)
+            time.sleep(0.0000075)
+            GPIO.output(self.pul_pin, True)
+            time.sleep(0.0000075)
 
-    	GPIO.output(self.enable_pin, False)
-    	time.sleep(0.000005)
+        GPIO.output(self.enable_pin, False)
+        time.sleep(0.000005)
 
 
 
