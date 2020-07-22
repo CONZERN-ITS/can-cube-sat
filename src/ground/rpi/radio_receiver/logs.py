1597361d85542b3c8a106d3248ab259cc5fe1600
dll31 <dania.litvinov31@yandex.ru>
2020-07-22 17:47:09 +0300

    Небольшие изменения

---
+++ src/ground/rpi/radio_receiver/logs.py
@@ -0,0 +1,37 @@
+import os
+import re
+import datetime
+
+
+# нахождение текущего времени и текущей даты
+def now_datetime_str():
+    now = str(datetime.datetime.now())
+    now = re.split(r'[\s.]', now)
+    return now[0] + "_" + now[1]
+
+
+def generate_new_logs_filename(logs_path, basename, extension):
+    """ генерация новых лог файлов """
+    datetime_str = now_datetime_str()
+    file_name = "{}_{}.{}".format(basename, datetime_str, extension)
+    return os.path.join(logs_path, file_name)
+
+
+def open_log_file(log_filename, mode):
+    try:
+        mavlink_packet_log = open(log_filename, str(mode))
+    except FileNotFoundError:
+        print("No such file or directory: {}".format(log_filename))
+        return
+
+    return mavlink_packet_log
+
+
+def make_logs_directory(logs_path):      # создание директории для логов
+    logs_path = str(logs_path)
+
+    try:
+        os.mkdir(logs_path)
+        print("Directory", logs_path, "created")
+    except FileExistsError:
+        print("Directory", logs_path, "already exists")
