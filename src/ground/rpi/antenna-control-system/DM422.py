e6d4fcb2a0f14b4ec47d7b91c135e0eb1f1573ad
Agnus290 <tenebris.agnus@yandex.ru>
2020-07-23 18:45:08 +0300

    Исправление драйвера и подготовка к тесту

--- src/ground/rpi/antenna-control-system/DM422.py
+++ src/ground/rpi/antenna-control-system/DM422.py
@@ -17,15 +17,20 @@ class DM422_control_client ():
         self.enable_pin = enable_pin
         self.gearbox_num = gearbox_num
         self.deg_per_step = deg_per_step
-        self.stop_triggers = []
+        self.pos_dir_stop_triggers = []
+        self.neg_dir_stop_triggers = []
         self.stop_state = stop_state
         self.last_steps_num = None
         self.last_steps_direction = None
 
-    def setup_stop_triggers(self, stop_triggers=[]):
-        self.stop_triggers = stop_triggers
+    def setup_stop_triggers(self, pos_dir_triggers=[], neg_dir_triggers=[]):
+        self.pos_dir_stop_triggers = pos_dir_triggers
+        self.neg_dir_stop_triggers = neg_dir_triggers
 
-        for pin in self.stop_triggers:
+        for pin in self.pos_dir_stop_triggers:
+            GPIO.setup(pin, GPIO.IN)
+
+        for pin in self.neg_dir_stop_triggers:
             GPIO.setup(pin, GPIO.IN)
 
     def setup (self):
@@ -55,33 +60,34 @@ class DM422_control_client ():
     def rotate_using_angle (self, ang):
         steps = self.angle_to_steps(ang)
         if ang > 0:
-            return self.rotate_using_steps(steps, False)
-        else:
             return self.rotate_using_steps(steps, True)
+        else:
+            return self.rotate_using_steps(steps, False)
 
     def rotate_using_steps (self, steps, direction=True):
-        trigger_num = None
         self.last_steps_num = steps
         self.last_steps_direction = direction
+        if direction:
+            triggers = self.pos_dir_stop_triggers
+        else:
+            triggers = self.neg_dir_stop_triggers
         GPIO.output(self.pul_pin, True)
         self.set_enable()
-        time.sleep(0.000005)
+        time.sleep(0.00001)
 
         GPIO.output(self.dir_pin, direction)
-        time.sleep(0.000005)
+        time.sleep(0.00001)
 
         for i in range(steps):
-            for pin in self.stop_triggers:
+            for pin in triggers:
                 if GPIO.input(pin) == self.stop_state:
-                    trigger_num = pin
                     self.last_steps_num = i
-                    break
+                    return pin
             GPIO.output(self.pul_pin, False)
-            time.sleep(0.0000075)
+            time.sleep(0.00001)
             GPIO.output(self.pul_pin, True)
-            time.sleep(0.0000075)
-
-        return trigger_num
+            time.sleep(0.00001)
+        return None
 
     def get_last_steps_num(self):
         return self.last_steps_num
@@ -90,8 +96,18 @@ class DM422_control_client ():
         return self.last_steps_direction
 
 if __name__ == '__main__':
-    dm422 = DM422_control_client(gearbox_num=102, deg_per_step=1.8)
+    dm422 = DM422_control_client(gearbox_num=102, deg_per_step=(1.8/8))
     dm422.setup()
-    dm422.setup_stop_triggers([7, 11])
+    dm422.setup_stop_triggers([11], [7])
     while True:
-        dm422.rotate_using_angle(90)
+        value1 = dm422.rotate_using_angle(360)
+        print(dm422.get_last_steps_num())
+        if value1 is not None:
+            print('stop --------')
+            break
+    while True:
+        value = dm422.rotate_using_angle(-360)
+        print(dm422.get_last_steps_num())
+        if (value is not None) and (value != value1):
+            print('end --------')
+            break
