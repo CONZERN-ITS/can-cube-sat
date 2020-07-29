b8c9f99de9943923a5ffefb32ce652ab50744031
dll31 <dania.litvinov31@yandex.ru>
2020-07-29 17:02:07 +0300

    Работает парсер логов с таймштампом. Необходима доработка для сырого лога

--- src/utility/log_parser.py
+++ src/utility/log_parser.py
@@ -1,10 +1,13 @@
-import serial 
 import os
+import sys
+
+
+import time
 
 import argparse
 
 os.environ["MAVLINK_DIALECT"] = "its"
-os.environ["MAVLINK20"] = "1"
+os.environ["MAVLINK20"] = "True"
 
 from pymavlink.dialects.v20 import its as its_mav
 from pymavlink import mavutil
@@ -13,17 +16,25 @@ from pymavlink import mavutil
 def arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-lp", "--logs_path", help="path to log file")
+
+    parser.add_argument("-ts", "--timestamps", help="if packet have timestamps before packet body", action="store_true", default=False)
     args = parser.parse_args()
     return args
 
 
-def read_log_file(path_to_log_file):
-	mav_read_file = mavutil.mavlogfile(path_to_log_file, robust_parsing=True, notimestamps=False)
+def read_log_file(path_to_log_file, timestamps):
+	mav_read_file = mavutil.mavlogfile(path_to_log_file, robust_parsing=True, notimestamps=(not timestamps))
 
 	while True:
 	    msg_read = mav_read_file.recv_match(blocking=False)
+	    if msg_read == None:
+	    	break
 	    print(msg_read)
 
-if __name__ == '__main__':
+def main():
 	args = arguments()
-	read_log_file(args.logs_path)
+	read_log_file(args.logs_path, args.timestamps)
+
+if __name__ == '__main__':
+	main()
+	sys.exit()
