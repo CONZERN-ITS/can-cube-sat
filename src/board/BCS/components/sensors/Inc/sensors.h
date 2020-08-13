068f4d60d197458f9a6391dcbcd22c95d32928ac
SeresHotes <sereshotes@gmail.com>
2020-08-13 21:41:33 +0300

    Драйверы для шины 1W и ds18b20, и управляющий таск
    Добавлены драйверы от https://github.com/DavidAntliff/ для работы с
    ds18b20. Пока что без тестов.
    
    Также взят его пример за основу для будущего таска, управляющего
    температурой. В будущем он будет отправлять данные в route, но пока
    он просто печатает все, что видит.

---
+++ src/board/BCS/components/sensors/Inc/sensors.h
@@ -0,0 +1,15 @@
+/*
+ * sensors.h
+ *
+ *  Created on: Aug 13, 2020
+ *      Author: sereshotes
+ */
+
+#ifndef COMPONENTS_SENSORS_INC_SENSORS_H_
+#define COMPONENTS_SENSORS_INC_SENSORS_H_
+
+
+
+
+
+#endif /* COMPONENTS_SENSORS_INC_SENSORS_H_ */
