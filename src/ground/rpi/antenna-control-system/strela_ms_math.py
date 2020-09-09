fc252882992da9bee1230d0a71a3093ebb6c276f
Agnus290 <tenebris.agnus@yandex.ru>
2020-09-09 19:12:53 +0300

    Добавление пересчета углов в градусы + исправление математики

--- src/ground/rpi/antenna-control-system/strela_ms_math.py
+++ src/ground/rpi/antenna-control-system/strela_ms_math.py
@@ -4,19 +4,20 @@ from math import sin, cos, radians, acos, asin
 def dec_to_top_matix(lat, lon):
     lat = radians(lat)
     lon = radians(lon)
-    return  NumPy.array([[-sin(lat)*cos(lon), -sin(lat)*sin(lon) , cos(lat)],
-                         [sin(lon), -cos(lon), 0],
-                         [cos(lat)*cos(lon), cos(lat)*sin(lon), sin(lat)]])
+    return  NumPy.array([[-sin(lat)*cos(lon),  -sin(lat)*sin(lon), cos(lat)],
+                         [ sin(lon),           -cos(lon),          0       ],
+                         [ cos(lat)*cos(lon),   cos(lat)*sin(lon), sin(lat)]])
 
 def top_to_gcscs_matix(mag, accel, decl):
     accel = accel / NumPy.linalg.norm(accel)
     mag = mag / NumPy.linalg.norm(mag)
+    decl = radians(decl)
 
-    mag = mag - accel * NumPy.dot(accel, mag)
+    mag = mag - accel * NumPy.dot(accel.T, mag)
     mag = mag / NumPy.linalg.norm(mag)
-    mag = mag * cos(decl) + accel * (1 - cos(decl)) * NumPy.dot(accel, mag) + sin(decl) * NumPy.cross(accel, mag)
+    mag = mag * cos(decl) + accel * (1 - cos(decl)) * NumPy.dot(accel.T, mag) + sin(decl) * NumPy.cross(accel.T, mag.T).T
 
-    b = NumPy.cross(accel, mag)
+    b = NumPy.cross(accel.T, mag.T).T
     b = b / NumPy.linalg.norm(b)
 
-    return NumPy.linalg.inv(NumPy.array([mag, b, accel]))
+    return NumPy.column_stack([mag, b, accel])
