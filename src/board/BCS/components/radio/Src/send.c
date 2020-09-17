6baed15dcb19fe9cf97ba40deb67fe10e89da38b
snork2 <strange.snork@gmail.com>
2020-09-18 01:33:35 +0300

    Реворк радио таска

--- src/board/BCS/components/radio/Src/send.c
+++ src/board/BCS/components/radio/Src/send.c
@@ -26,16 +26,11 @@ typedef struct  {
 /*
  * Буфер всех заданных сообщений
  */
-#define F(x,a,b) {a, b, -b, 0, {0}},
-static msg_container arr_id[] = {
+#define F(__msg_hash, __msg_id, __period) {__msg_id, __period, -__period, 0, {0}},
+static const msg_container arr_id[] = {
 		RADIO_SEND_ID_ARRAY(F)
 };
 #undef F
-
-//Устанавливаем первое last в ноль, чтобы в начале отпрвлялись
-//только нормальные сообщения
-msg_container default_msg = {0, RADIO_DEFAULT_PERIOD, 0, 0, {0}};
-
 
 /*
  * Хэш-функция для получения индекса в массиве arr_id для заданного
@@ -53,6 +48,20 @@ static int get_hash(int id) {
 		return -1;
 	}
 #undef F
+}
+
+//! Проверка на то, что сообщение забанено на отправку по радио
+/*! Вернет 0, если не забанено и не 0, если забанено */
+static int is_msg_banned(int msg_id)
+{
+#define F(__msg_id) case __msg_id: return 1;
+	switch (msg_id) {
+	RADIO_SEND_BAN(F)
+	default: break;
+	}
+#undef F
+
+	return 0;
 }
 
 #define RADIO_SEND_BUF_SIZE 30
@@ -76,16 +85,16 @@ static msg_container* find(const mavlink_message_t *msg) {
  * Создание контейнера в буфере arr_buf для данного сообщения
  */
 static int add(const mavlink_message_t *msg) {
+
+	if (is_msg_banned(msg->msgid)) {
+		return 2;
+	}
+
 	if (arr_buf_size >= RADIO_SEND_BUF_SIZE) {
 		ESP_LOGE("radio", "No free space for new msg");
 		return 1;
 	}
-#define F(a) case a: return 2;
-	switch (msg->msgid) {
-	RADIO_SEND_BAN(F)
-	default: break;
-	}
-#undef F
+
 	ESP_LOGI("radio", "Add: %d %d:%d", msg->msgid, msg->sysid, msg->compid);
 	int id = get_hash(msg->msgid);
 	if (id >= 0) {
@@ -144,23 +153,7 @@ static msg_container *get_best(int now) {
 	return st_best;
 }
 
-static void task_recv(void *arg) {
-	its_rt_task_identifier tid = {
-			.name = "radio_send"
-	};
-	//Регистрируем на сообщения всех типов
-	tid.queue = xQueueCreate(10, MAVLINK_MAX_PACKET_LEN);
-	its_rt_register_for_all(tid);
 
-
-	while (1) {
-		mavlink_message_t msg;
-		//Ожидаем получения сообщения
-		xQueueReceive(tid.queue, &msg, portMAX_DELAY);
-
-		update_msg(&msg);
-	}
-}
 /*
  * Настройки safe_uart_send
  */
@@ -177,113 +170,149 @@ typedef struct  {
  */
 typedef struct  {
 	safe_send_cfg_t cfg; //Настройки
-	int32_t filled;	//Заполненность буферв
-	int64_t last_checked; //Время в мс последнего изменения filled
+	int filled;	//Заполненность буферв
+	int64_t last_checked; //Время в мкс последнего изменения filled
 } safe_send_t;
 
-/*
- * Безопасная отправка данных через уарт. Необходимо инициализировать
- * h->cfg.
- *
- * Безопасность заключается в отпрвке данных с такой частотой, чтобы
- * не превысить отправляющую способность устройства по ту сторону от
- * уарта.
- *
- * Границы буфера позволяют отправлять данные пачками
- */
-static void safe_uart_send(safe_send_t *h, uint8_t *buf, uint16_t size) {
-	uint32_t Bs = h->cfg.baud_send / 8; //Перевод из бит/сек в Байт/сек
-
-	//Буфер успел освободиться за то время, пока эта ф-ия не вызывалась
-	int64_t now = esp_timer_get_time();
-	h->filled -= ((now - h->last_checked) * Bs) / 1000000;
-	h->filled = h->filled > 0 ? h->filled : 0;
-
-
-	while (size > 0) {
-		ESP_LOGD("radio", "Cycle 1 %d", h->filled);
-		if (h->filled >= h->cfg.high_thrld) {
-			//Если буфер достаточно заполнен, то можно пока не отправлять.
-			uint32_t ttt = Bs * portTICK_PERIOD_MS;
-			uint32_t ticks = ((h->filled - h->cfg.low_thrld) * 1000 + ttt - 1) / ttt;
-			vTaskDelay(ticks);
-			h->filled -= ticks * portTICK_PERIOD_MS * Bs / 1000;
-		} else {
-			uint32_t maxSend = (h->cfg.buffer_size - h->filled);
-
-			uint16_t s = size;
-			if (size >= maxSend) {
-				s = maxSend;
-			}
-			int64_t start = esp_timer_get_time();
-			uart_write_bytes(h->cfg.port, (char *) buf, s);
-
-			int t = uart_wait_tx_done(h->cfg.port, RADIO_SEND_DELAY / portTICK_PERIOD_MS);
-			int64_t now = esp_timer_get_time();
-
-			h->filled -= (Bs * (now - start)) / 1000000;
-			h->filled += s;
-			h->filled = h->filled > 0 ? h->filled : 0;
-
-			buf += s;
-			size -= s;
-
-		}
+static 	safe_send_t sst = {
+	.cfg = {
+			.low_thrld = 0,
+			.high_thrld = 100,
+			.baud_send = 62500 / 2,
+			.buffer_size = 1000,
+			.port = ITS_UARTR_PORT
 	}
-	h->last_checked = esp_timer_get_time();
+};
 
-}
+
 static void task_send(void *arg) {
-	safe_send_t sst = {0};
-	sst.cfg.low_thrld = 0;
-	sst.cfg.high_thrld = 100;
-	sst.cfg.baud_send = 62500 / 2;
-	sst.cfg.buffer_size = 1000;
-	sst.cfg.port = ITS_UARTR_PORT;
+	// Конфигурация отправки пакетов с контроллируемой скоростью
+
+	// Регистрация таска в маршрутизаторе
+	its_rt_task_identifier tid = {
+			.name = "radio_send"
+	};
+	//Регистрируем на сообщения всех типов
+	tid.queue = xQueueCreate(10, MAVLINK_MAX_PACKET_LEN);
+	its_rt_register_for_all(tid);
 
 
 	//Количество отправленных сообщений
 	int msg_count = 0;
+	// Буфер для отправляемого сообщения
+	uint8_t out_buf[MAVLINK_MAX_PACKET_LEN] = {0};
+
+	// Текущая отправляемая порция
+	uint8_t * portion = out_buf;
+	// размер порции
+	int portion_size = 0;
+
+
 	while (1) {
-
-		ESP_LOGD("radio", "Cycle 0 %d", msg_count);
-		msg_container *st = 0;
-		while (1) {
-			st = get_best(msg_count);
-
-			if (st == 0) {
-				//Нет сообщений, которые можно было бы отправить! Подождем...
-				vTaskDelay(RADIO_SEND_DELAY / portTICK_PERIOD_MS);
-			} else {
-				break;
-			}
+		// Пробуем получить новое сообщение из очереди
+		// Блокируемся ненадолго, чтобы равномерно отправлять сообщения из очереди
+		mavlink_message_t incoming_msg = {0};
+		BaseType_t received = xQueueReceive(tid.queue, &incoming_msg, 200 / portTICK_PERIOD_MS);
+		if (pdTRUE == received)
+		{
+			// Если мы получили сообщение - складываем в его хранилище
+			update_msg(&incoming_msg);
 		}
-		uint8_t buf[MAVLINK_MAX_PACKET_LEN] = {0};
-		int count = mavlink_msg_to_send_buffer(buf, &st->last_msg);
-		st->is_updated = 0; //Сообщение внутри контейнера уже не свежее
-		st->last = msg_count;//Запоминаем, когда сообщение было отправленно в последний раз
-		/*
-		for (int i = 0; i < count; i++) {
-			buf[i] = i;
-		}*/
-		safe_uart_send(&sst, buf, count);
 
-		msg_count++;
-	}
+		// Нам есть чего отправлять?
+		if (0 == portion_size)
+		{
+			// Нет, нету. Готовим.
+			msg_container *st = 0;
+			st = get_best(msg_count);
+			if (0 == st)
+			{
+				// Нет сообщений, Пойдем получать следующее
+				ESP_LOGI("radio", "message buffer is empty");
+				continue;
+			}
+
+			portion_size = mavlink_msg_to_send_buffer(out_buf, &st->last_msg);
+			portion = out_buf;
+
+			st->is_updated = 0; // Сообщение внутри контейнера уже не свежее
+			st->last = msg_count;// Запоминаем, когда сообщение было отправленно в последний раз
+			msg_count++;
+		}
+
+		// Отлично, мы готовы отправлять. Сколько там радио может принять?
+		const int Bs = sst.cfg.baud_send / 8; //Перевод из бит/сек в Байт/сек
+		//Буфер успел освободиться за то время, пока эта ф-ия не вызывалась
+		const int64_t now = esp_timer_get_time();
+		// Сколько байт успело уйти из буфера радио
+		int64_t drained_bytes = ((now - sst.last_checked) * Bs) / (1000*1000);
+		if (drained_bytes >= sst.filled)
+			sst.filled = 0;
+		else
+			sst.filled -= drained_bytes;
+		sst.last_checked = now;
+
+		int64_t radio_free_space = sst.cfg.buffer_size - sst.filled;
+		if (radio_free_space < 0)
+			radio_free_space = 0;
+
+		if (radio_free_space < sst.cfg.low_thrld)
+		{
+			// Слишком мало. Подождем
+			continue;
+		}
+
+		// Теперь понятно сколько мы можем и сколько хотим отправить
+		int to_write = portion_size;
+		if (to_write > radio_free_space)
+			to_write = (int)radio_free_space;
+
+		if (to_write > 0)
+		{
+			int rc = uart_write_bytes(sst.cfg.port, (char*)portion, to_write);
+			if (rc < 0)
+			{
+				// Что-то не то с записью
+				ESP_LOGE("radio", "error on uart write: %d", rc);
+				// Сбросим этот пакет, так как он всеравно устареет
+				portion_size = 0;
+				// Пойдем дальше, что делать
+				continue;
+			}
+			else if (rc == 0)
+			{
+				// Кажется, мы пишем слишком быстро, что фифо за нами не успевает?
+				ESP_LOGW("radio", "tx fifo overflow");
+				continue;
+			}
+
+			// Сдвигаем указатели на количество записанного
+			portion += rc;
+			portion_size -= rc;
+
+			// эти байты как заполненные в буфере радио
+			sst.filled += rc;
+		}
+	} // while(1)
 	vTaskDelete(NULL);
 }
-static TaskHandle_t task_s;
-static TaskHandle_t task_r;
+
+
+static TaskHandle_t task_s = NULL;
+
+
 void radio_send_suspend(void) {
-	vTaskSuspend(task_s);
+	if (task_s != NULL)
+		vTaskSuspend(task_s);
 }
+
 
 void radio_send_resume(void) {
-	vTaskResume(task_s);
+	if (task_s != NULL)
+		vTaskResume(task_s);
 }
 
-void radio_send_init(void) {
 
-	xTaskCreatePinnedToCore(task_send, "Radio send", configMINIMAL_STACK_SIZE + 4000, 0, 3, &task_s, tskNO_AFFINITY);
-	xTaskCreatePinnedToCore(task_recv, "Radio buf", configMINIMAL_STACK_SIZE + 4000, 0, 4, &task_r, tskNO_AFFINITY);
+void radio_send_init(void) {
+	xTaskCreatePinnedToCore(task_send, "Radio send", configMINIMAL_STACK_SIZE + 4000, 0, 4, &task_s, tskNO_AFFINITY);
 }
